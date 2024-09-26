5a2dd481f04629b71a8af5ee1150025832388eb4
Banri Yasui <69246274+jomspk@users.noreply.github.com>
2024-09-26 13:53:33 +0900

    delete: フロントのcardsとusersの削除

--- webapp/src/app/cards/[id]/edit/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CardEditPage } from "@/features/card/CardEditPage";
-
-export default function CardEdit({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
-  return <CardEditPage id={id} />;
-}
--- webapp/src/app/cards/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { CardDetailPage } from "@/features/card/CardDetailPage";
-
-export default function Card({ params: { id } }: { params: { id: string } }) {
-  return <CardDetailPage id={id} />;
-}
