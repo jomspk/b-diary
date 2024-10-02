1b637eb5a12d84bd9731d5d2953de6c61b573665
Banri Yasui <69246274+jomspk@users.noreply.github.com>
2024-10-03 00:00:06 +0900

    Update middleware.ts

--- webapp/src/middleware.ts
+++ webapp/src/middleware.ts
@@ -5,7 +5,6 @@ import { getSession } from "@auth0/nextjs-auth0/edge";
 export async function middleware(request: NextRequest) {
   const session = await getSession(request, NextResponse.next());
   const url = request.nextUrl.clone();
-  console.log("session", session);
   if (!session) {
     url.pathname = "/login";
     return NextResponse.redirect(url);
