4bfa207fb4af145d37f7c8e5089edde7560b0053
Fironn <shihomihashi@gmail.com>
2024-10-19 17:13:41 +0900

    利用規約追加

--- webapp/src/app/login/page.tsx
+++ webapp/src/app/login/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -6,8 +8,13 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Checkbox, CheckboxLabel } from "@/components/ui/checkbox";
+import Terms from "@/features/login/Terms";
+import { CheckedState } from "@radix-ui/react-checkbox";
+import { useState } from "react";
 
 export default function Component() {
+  const [onChecked, setOnChecked] = useState<CheckedState>(false);
   return (
     <div className="bg-[url('/kohaku_background.jpg')] bg-cover bg-center min-h-full w-full flex items-center justify-center p-4">
       <Card className="w-3/5 min-h-96 flex items-center justify-center flex-col p-10">
@@ -21,9 +28,18 @@ export default function Component() {
             当社の製品は、あなたの生産性を向上させ、ワークフローを効率化します。
             今すぐログインして、素晴らしい機能をお試しください！
           </p>
+          <Terms />
+          <div className="flex items-center space-x-2 mt-4">
+            <Checkbox
+              id="terms"
+              checked={onChecked}
+              onCheckedChange={setOnChecked}
+            />
+            <CheckboxLabel htmlFor="terms">同意する</CheckboxLabel>
+          </div>
         </CardContent>
         <CardFooter className="flex justify-center">
-          <Button className="w-full sm:w-auto">
+          <Button className="w-full sm:w-auto" disabled={!onChecked}>
             <a href="/api/auth/login">ログイン</a>
           </Button>
         </CardFooter>
---
+++ webapp/src/features/login/Terms.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
+
+export default function Terms() {
+  const [markdown, setMarkdown] = useState("");
+
+  const components = {
+    h1: ({ ...props }) => <h1 className="my-4">{props.children}</h1>,
+    h2: ({ ...props }) => <h2 className="mt-4 mb-2">{props.children}</h2>,
+  };
+
+  useEffect(() => {
+    fetch(`/terms.md`)
+      .then((m) => {
+        return m.text();
+      })
+      .then((md) => {
+        setMarkdown(md);
+      });
+  }, []);
+
+  return (
+    <ScrollArea className="h-[300px] scroll-smooth mt-4 border-2 rounded-sm">
+      <article className="prose flex-1">
+        <ReactMarkdown className="py-6 px-4" components={components}>
+          {markdown}
+        </ReactMarkdown>
+      </article>
+    </ScrollArea>
+  );
+}
