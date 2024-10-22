b027e3a6cfec370e0cc19d7470db42c9c151336b
Banri Yasui <69246274+jomspk@users.noreply.github.com>
2024-10-22 17:14:38 +0900

    update: logoの変更

--- webapp/src/app/login/page.tsx
+++ webapp/src/app/login/page.tsx
@@ -18,15 +18,11 @@ export default function Component() {
   return (
     <div className="bg-[url('/kohaku_background.jpg')] bg-cover bg-center min-h-full w-full flex ">
       <div className="bg-white/75 flex items-center justify-center p-4 flex-grow">
-        <Card className="w-full max-w-[720px] min-h-90 flex items-center justify-center flex-col py-[40px] md:py-[80px] px-1 gap-[40px]">
+        <Card className="w-full max-w-[720px] min-h-90 flex items-center justify-center flex-col py-[40px] md:py-[80px] px-1">
           <CardHeader>
             <Image src="/logo_text.svg" alt="琥珀" width={202} height={57} />
           </CardHeader>
-          <CardContent className="w-full max-w-[448px]">
-            <p>
-              当社の製品は、あなたの生産性を向上させ、ワークフローを効率化します。
-              今すぐログインして、素晴らしい機能をお試しください！
-            </p>
+          <CardContent className="w-full max-w-[480px]">
             <Terms />
           </CardContent>
           <CardContent>
